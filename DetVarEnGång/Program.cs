7e4e634c97958c13544f5eea854e18979a276755
Ludwig <ludde-j@hotmail.com>
2018-09-11 14:29:36 +0200

    hej

--- DetVarEnGång/Program.cs
+++ DetVarEnGång/Program.cs
@@ -9,8 +9,24 @@ namespace DetVarEnGång
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Det var en gång en hund");
+        {   // Inmatning av favorit-saker.
+
+            Console.WriteLine("Välkommen!");
+            Console.WriteLine("Vad är din favoritfärg?");
+            string färg = Console.ReadLine();
+            Console.WriteLine("Vad är ditt favoritland?");
+            string land = Console.ReadLine();
+            Console.WriteLine("Vad är din favoriträtt?");
+            string rätt = Console.ReadLine();
+            Console.WriteLine("Vad är din favoritsport?");
+            string sport = Console.ReadLine();
+
+
+            // Börja berättelse här.
+
+            
+
+            Console.ReadKey();
         }
     }
 }
